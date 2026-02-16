a4e7ebea2f8dbe38de4ef57863e250509542e791
Hariharan2716 <hariharan.m2716@gmail.com>
2026-02-16 15:13:21 +0530

    Started the home page worked with App.jsx file and created components folder with Navbar component and Herocomponent which helped me to understand the working of react component which gives the knowledge of working components

---
+++ react-jobs/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../assets/image/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-indigo-700 border-b border-indigo-500">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-center md:items-stretch md:justify-start">
+          <a href="/"><img src={logo} alt="React Jobs" className="h-10 w-auto" />
+            <span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span>
+          </a>
+          <div className="md:ml-auto">
+            <div className="flex space-x-2">
+              <a href="#">Home</a>
+              <a href="#">Jobs</a>
+              <a href="#">Add Job</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
